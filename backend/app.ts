a2474c4ccac6bd4ad879a789465b6184aae8986e
SecretariatV <henry.campbell.1225@gmail.com>
2024-04-20 06:23:13 +0900

    create: user authentication routes

--- backend/app.ts
+++ backend/app.ts
@@ -3,12 +3,26 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import testDataRoutes from "./routes/testdata-routes";
 import { getBackendPort } from "../src/utils/portUtils";
+import session from "express-session";
+import passport from "passport";
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.use(
+  session({
+    secret: "session secret",
+    resave: false,
+    saveUninitialized: false,
+    unset: "destroy",
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 if (process.env.NODE_ENV === "test" || process.env.NODE_ENV === "development") {
   app.use("/testData", testDataRoutes);
---
+++ backend/routes/auth.ts
@@ -0,0 +1,38 @@
+import express, { Request, Response } from "express";
+import passport from "passport";
+import { deflate } from "zlib";
+
+const router = express.Router();
+
+// authentication routes
+router.post(
+  "/login",
+  passport.authenticate("local"),
+  (req: Request, res: Response): void => {
+    if (req.body.remember) {
+      req.session!.cookie.maxAge = 24 * 60 * 60 * 1000 * 30; // Expire in 30 days
+    } else {
+      req.session!.cookie.expires = undefined;
+    }
+
+    res.send({ user: req.user });
+  }
+);
+
+router.post("/logout", (req: Request, res: Response): void => {
+  res.clearCookie("connect.sid");
+  req.logout(() => res.redirect("/"));
+  req.session!.destroy(function () {
+    res.redirect("/");
+  });
+});
+
+router.get("/checkAuth", (req, res) => {
+  if (!req.user) {
+    res.status(401).json({ error: "User is unauthorized" });
+  } else {
+    res.status(200).json({ user: req.user });
+  }
+});
+
+export default router;
