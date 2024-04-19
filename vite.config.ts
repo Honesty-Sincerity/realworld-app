98c67588ae97978b46d828b8823c1d035cdeed6f
SecretariatV <henry.campbell.1225@gmail.com>
2024-04-20 01:44:55 +0900

    basic structure

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig(({ command, mode }) => {
+  return {
+    plugins: [react()],
+    server: {
+      port: 4000,
+    },
+    build: {
+      outDir: "build",
+      sourcemap: true,
+    },
+  };
+});
